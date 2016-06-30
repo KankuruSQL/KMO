451aaaf60242ad84c1c8e96f12b6dc47adec7dea
GrÃ©gory Boge <g.boge@criteo.com>
2016-06-30 08:14:36 +0200

    Extend smo.Server with Instance Version

---
+++ Examples/PrintVersions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using KMO;
+
+namespace PrintVersions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In this example, you can get your instance version
+            ServerConnection myCnx = new ServerConnection("localhost");
+            Server myServer = new Server(myCnx);
+            Console.WriteLine("Version : " + myServer.VersionString());
+            Console.WriteLine("Full Version : " + myServer.VersionFull());
+            myCnx.Disconnect();
+            Console.Read();
+        }
+    }
+}
---
+++ KServer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using smo = Microsoft.SqlServer.Management.Smo;
+
+namespace KMO
+{
+    public static class KServer
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string VersionString(this smo.Server s)
+        {
+            string version;
+            switch (s.VersionMajor.ToString() + "." + s.VersionMinor.ToString())
+            {
+                case "13.0":
+                    version = "Sql Server 2016";
+                    break;
+                case "12.0":
+                    version = "Sql Server 2014";
+                    break;
+                case "11.0":
+                    version = "Sql Server 2012";
+                    break;
+                case "10.50":
+                    version = "Sql Server 2008 R2";
+                    break;
+                case "10.0":
+                    version = "Sql Server 2008";
+                    break;
+                case "9.0":
+                    version = "Sql Server 2005";
+                    break;
+                case "8.0":
+                    version = "Sql Server 2000";
+                    break;
+                case "7.0":
+                    version = "Sql Server 7";
+                    break;
+                default:
+                    version = "Unknown edition";
+                    break;
+            }
+            return version;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string VersionFull(this smo.Server s)
+        {
+            return string.Format("{0} {1} {2} ({3})", s.VersionString(), s.ProductLevel, s.Edition, s.Version);
+        }
+
+    }
+}
