6914fabdeee185c5889d3ff020e60519c8800ccb
GrÃ©gory Boge <g.boge@criteo.com>
2016-07-04 07:53:00 +0200

    new Backup and security methods

--- KDatabase.cs
+++ KDatabase.cs
@@ -9,11 +9,90 @@ namespace KMO
 {
     public static class KDatabase
     {
+        #region Replication
+        /// <summary>
+        /// Return true is the database is a distributor database
+        /// </summary>
+        /// <param name="d">your smo database</param>
+        /// <returns>a bool</returns>
         public static bool IsDistributor(this smo.Database d)
         {
             string sql = string.Format("SELECT 1 FROM sys.databases WHERE is_distributor = 1 AND name = '{0}'", d.Name);
             DataSet ds = d.ExecuteWithResults(sql);
             return (ds.Tables[0].Rows.Count == 1);
         }
+        #endregion
+
+        #region Backups
+        /// <summary>
+        /// Get the backup history for a given database
+        /// </summary>
+        /// <param name="d">your smo database</param>
+        /// <param name="limit">This param will be used in the SELECT TOP XXX</param>
+        /// <returns>a dataset with the result of the query</returns>
+        public static DataSet GetBackupHistory(this smo.Database d, int limit = 1000)
+        {
+            string sql = string.Format(@"SELECT TOP {0} 
+    CASE s.[type]
+        WHEN 'D' THEN 'Full'
+        WHEN 'I' THEN 'Differential'
+        WHEN 'L' THEN 'Transaction Log'
+        ELSE 'Other' 
+	END AS BackupType
+    , m.physical_device_name
+    , CAST(CAST(s.compressed_backup_size / 1048576 AS INT) AS VARCHAR(20)) + ' ' + 'MB' AS bkSize
+    , CAST(CAST(100 - (compressed_backup_size / backup_size * 100) as DECIMAL(9,2)) as VARCHAR(10)) + ' %' AS ratiocompression
+    , CAST(DATEDIFF(SECOND, s.backup_start_date, s.backup_finish_date) AS VARCHAR(5)) + ' ' + 'Seconds' AS TimeTaken
+    , s.backup_start_date
+    , s.backup_finish_date
+	, s.first_lsn AS first_lsn
+	, s.last_lsn AS last_lsn
+    , CAST(s.compressed_backup_size / 1048576 as INT) as bkSizeInt
+    , DATEDIFF(second, s.backup_start_date, s.backup_finish_date) TimeTakenInt
+    , CASE s.[type]
+        WHEN 'D' THEN '#32FF0000'
+        WHEN 'I' THEN '#320AFF0E'
+        WHEN 'L' THEN '#32FFFF00'
+        ELSE '#00000000'
+    END AS rowColor
+FROM msdb.dbo.backupset s (NOLOCK)
+    INNER JOIN msdb.dbo.backupmediafamily m (NOLOCK) ON s.media_set_id = m.media_set_id
+WHERE s.database_name = '{1}'
+ORDER BY backup_start_date DESC
+    , backup_finish_date", limit, d.Name);
+            if (d.Parent.VersionMajor < 10)
+            {
+                sql = string.Format(@"SELECT TOP {0}
+    CASE s.[type]
+        WHEN 'D' THEN 'Full'
+        WHEN 'I' THEN 'Differential'
+        WHEN 'L' THEN 'Transaction Log'
+        ELSE 'Other'
+    END AS BackupType
+    , m.physical_device_name
+    , CAST(CAST(s.backup_size / 1048576 AS INT) AS VARCHAR(20)) + ' ' + 'MB' AS bkSize
+    , '-' as ratiocompression
+    , CAST(DATEDIFF(SECOND, s.backup_start_date, s.backup_finish_date) AS VARCHAR(5)) + ' ' + 'Seconds' AS TimeTaken,
+    , s.backup_start_date
+    , s.backup_finish_date
+	, s.first_lsn AS first_lsn
+	, s.last_lsn AS last_lsn
+    , CAST(s.backup_size / 1048576 as INT) as bkSizeInt
+    , DATEDIFF(second, s.backup_start_date, s.backup_finish_date) TimeTakenInt
+    , CASE s.[type]
+        WHEN 'D' THEN '#32FF0000'
+        WHEN 'I' THEN '#320AFF0E'
+        WHEN 'L' THEN '#32FFFF00'
+        ELSE '#00000000'
+    END AS rowColor
+FROM msdb.dbo.backupset s (NOLOCK)
+    INNER JOIN msdb.dbo.backupmediafamily m (NOLOCK) ON s.media_set_id = m.media_set_id
+WHERE s.database_name = {1}
+ORDER BY backup_start_date DESC
+	, backup_finish_date", limit, d.Name);
+            }
+            return d.ExecuteWithResults(sql);
+        }
+        #endregion
     }
 }
--- KServer.cs
+++ KServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using smo = Microsoft.SqlServer.Management.Smo;
@@ -8,6 +9,7 @@ namespace KMO
 {
     public static class KServer
     {
+        #region SQL Server Versions
         /// <summary>
         /// Get Sql server version name from major and minor version
         /// </summary>
@@ -59,5 +61,51 @@ namespace KMO
             return string.Format("{0} {1} {2} ({3})", s.VersionName(), s.ProductLevel, s.Edition, s.Version);
         }
 
+        #endregion
+
+        #region Backups
+        /// <summary>
+        /// For each DB, get the recovery model, the last backup full and the last restore
+        /// </summary>
+        /// <param name="s">your smo server</param>
+        /// <returns>a dataset with 1 datatable containing the result of the query</returns>
+        public static DataSet GetBackupHistory(this smo.Server s)
+        {
+            smo.Database d = s.Databases["msdb"];
+            return d.ExecuteWithResults(@"SELECT sdb.name AS DatabaseName
+    , MAX(sdb.recovery_model_desc) as recoveryModel
+    , MAX(bus.backup_finish_date) AS LastBackUpTime
+	, MAX(rh.restore_date) as LastRestoreTime
+FROM sys.databases sdb (NOLOCK)
+    LEFT OUTER JOIN msdb.dbo.backupset bus (NOLOCK) ON bus.database_name = sdb.name 
+		AND COALESCE(bus.is_snapshot, 0) != 1 
+		AND COALESCE(bus.type, 'D') = 'D' 
+    LEFT join msdb.dbo.restorehistory rh (NOLOCK) on rh.destination_database_name = sdb.Name
+GROUP BY sdb.name
+ORDER BY sdb.name");
+        }
+        #endregion
+
+        #region Logins and Security
+        /// <summary>
+        /// Get all login with sysadmin rights
+        /// </summary>
+        /// <param name="s">your smo server</param>
+        /// <returns>the result of the query</returns>
+        public static DataSet GetWhoIsSa(this smo.Server s)
+        {
+            smo.Database d = s.Databases["master"];
+            return d.ExecuteWithResults(@"SELECT mem.name
+	, mem.type_desc
+	, mem.create_date
+	, mem.modify_date
+	, mem.is_disabled
+FROM sys.server_role_members AS srm (NOLOCK)
+	INNER JOIN sys.server_principals AS mem (NOLOCK) ON mem.principal_id = srm.member_principal_id
+	INNER JOIN sys.server_principals AS rol (NOLOCK) ON rol.principal_id = srm.role_principal_id
+WHERE rol.name = 'sysadmin'
+ORDER BY mem.name");
+        }
+        #endregion
     }
 }
