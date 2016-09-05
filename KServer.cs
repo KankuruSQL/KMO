bc9abb3018207e9e064f607894c62bab97988da5
GrÃ©gory Boge <g.boge@criteo.com>
2016-09-05 08:00:37 +0200

    Add wait statistics mathod

--- KServer.cs
+++ KServer.cs
@@ -412,5 +412,52 @@ ORDER BY EventTime DESC";
         }
         #endregion
 
+        #region Waits
+        /// <summary>
+        /// Get Wait statistics for an instance. Very helpful to understand performance issues
+        /// </summary>
+        /// <param name="s">your smo server</param>
+        /// <param name="listWaitToIgnore">List of wait stats to ignore with simple quote and separated by coma. Example : 'WaitStatToIgnore1', 'WaitStatToIgnore2' </param>
+        /// <returns>return the result of the query in a DataTable</returns>
+        public static DataTable GetWaitStatistics(this smo.Server s, string listWaitToIgnore = "'BROKER_EVENTHANDLER', 'BROKER_RECEIVE_WAITFOR', 'BROKER_TASK_STOP', 'BROKER_TO_FLUSH', 'BROKER_TRANSMITTER', 'CHECKPOINT_QUEUE', 'CHKPT', 'CLR_AUTO_EVENT', 'CLR_MANUAL_EVENT', 'CLR_SEMAPHORE', 'DBMIRROR_DBM_EVENT', 'DBMIRROR_EVENTS_QUEUE', 'DBMIRROR_WORKER_QUEUE', 'DBMIRRORING_CMD', 'DIRTY_PAGE_POLL', 'DISPATCHER_QUEUE_SEMAPHORE', 'EXECSYNC', 'FSAGENT', 'FT_IFTS_SCHEDULER_IDLE_WAIT', 'FT_IFTSHC_MUTEX', 'HADR_CLUSAPI_CALL', 'HADR_FILESTREAM_IOMGR_IOCOMPLETION', 'HADR_LOGCAPTURE_WAIT', 'HADR_NOTIFICATION_DEQUEUE', 'HADR_TIMER_TASK', 'HADR_WORK_QUEUE', 'KSOURCE_WAKEUP', 'LAZYWRITER_SLEEP', 'LOGMGR_QUEUE', 'ONDEMAND_TASK_QUEUE', 'PWAIT_ALL_COMPONENTS_INITIALIZED', 'QDS_PERSIST_TASK_MAIN_LOOP_SLEEP', 'QDS_CLEANUP_STALE_QUERIES_TASK_MAIN_LOOP_SLEEP', 'REQUEST_FOR_DEADLOCK_SEARCH', 'RESOURCE_QUEUE', 'SERVER_IDLE_CHECK', 'SLEEP_BPOOL_FLUSH', 'SLEEP_DBSTARTUP', 'SLEEP_DCOMSTARTUP', 'SLEEP_MASTERDBREADY', 'SLEEP_MASTERMDREADY', 'SLEEP_MASTERUPGRADED', 'SLEEP_MSDBSTARTUP', 'SLEEP_SYSTEMTASK', 'SLEEP_TASK', 'SLEEP_TEMPDBSTARTUP', 'SNI_HTTP_ACCEPT', 'SP_SERVER_DIAGNOSTICS_SLEEP', 'SQLTRACE_BUFFER_FLUSH', 'SQLTRACE_INCREMENTAL_FLUSH_SLEEP', 'SQLTRACE_WAIT_ENTRIES', 'WAIT_FOR_RESULTS', 'WAITFOR', 'WAITFOR_TASKSHUTDOWN', 'WAIT_XTP_HOST_WAIT', 'WAIT_XTP_OFFLINE_CKPT_NEW_LOG', 'WAIT_XTP_CKPT_CLOSE', 'XE_DISPATCHER_JOIN', 'XE_DISPATCHER_WAIT', 'XE_TIMER_EVENT'")
+        {
+            smo.Database d = s.Databases["master"];
+            string sqlIgnore = string.Empty;
+            if (!string.IsNullOrEmpty(listWaitToIgnore))
+            {
+                sqlIgnore = "AND [wait_type] NOT IN (" + listWaitToIgnore + ")";
+            }
+            string sql = string.Format(@"WITH [Waits] AS
+(
+	SELECT
+		[wait_type],
+		[wait_time_ms] / 1000.0 AS [WaitS],
+		([wait_time_ms] - [signal_wait_time_ms]) / 1000.0 AS [ResourceS],
+		[signal_wait_time_ms] / 1000.0 AS [SignalS],
+		[waiting_tasks_count] AS [WaitCount],
+		100.0 * [wait_time_ms] / SUM ([wait_time_ms]) OVER() AS [Percentage],
+		ROW_NUMBER() OVER(ORDER BY [wait_time_ms] DESC) AS [RowNum]
+	FROM sys.dm_os_wait_stats
+	WHERE [waiting_tasks_count] > 0
+		{0}
+)
+SELECT
+	MAX ([W1].[wait_type]) AS [wait_type],
+	CAST (MAX ([W1].[WaitS]) AS DECIMAL (16,2)) AS [wait_time_s],
+	CAST (MAX ([W1].[ResourceS]) AS DECIMAL (16,2)) AS [Resource_S],
+	CAST (MAX ([W1].[SignalS]) AS DECIMAL (16,2)) AS [Signal_S],
+	MAX ([W1].[WaitCount]) AS [waiting_tasks_count],
+	CAST (MAX ([W1].[Percentage]) AS DECIMAL (5,2)) AS [pct],
+	CAST ((MAX ([W1].[WaitS]) / MAX ([W1].[WaitCount])) AS DECIMAL (16,4)) AS [AvgWait_S],
+	CAST ((MAX ([W1].[ResourceS]) / MAX ([W1].[WaitCount])) AS DECIMAL (16,4)) AS [AvgRes_S],
+	CAST ((MAX ([W1].[SignalS]) / MAX ([W1].[WaitCount])) AS DECIMAL (16,4)) AS [AvgSig_S]
+FROM [Waits] AS [W1]
+	INNER JOIN [Waits] AS [W2] ON [W2].[RowNum] <= [W1].[RowNum]
+GROUP BY [W1].[RowNum]
+HAVING SUM ([W2].[Percentage]) - MAX ([W1].[Percentage]) < 99;", sqlIgnore);
+            return d.ExecuteWithResults(sql).Tables[0];
+        }
+        #endregion
+
     }
 }
