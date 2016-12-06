c7251cc1b92bf33ea9ae4b07efe571b71e9104e0
GrÃ©gory Boge <g.boge@criteo.com>
2016-12-06 09:09:21 +0100

    GetTableTreeMaps method for a given database

--- KDatabase.cs
+++ KDatabase.cs
@@ -614,5 +614,78 @@ ORDER BY [Index_Advantage] DESC";
         }
         #endregion
 
+
+        public static DataTable GetTableTreeMaps(this smo.Database d, int minSize, int maxSize, bool withTables, bool withViews)
+        {
+            string objectTypeFilter = string.Empty;
+            if (withTables == false || withViews == false)
+            {
+                if (!withTables)
+                {
+                    objectTypeFilter = " and type != 'U' ";
+                }
+                if (!withViews)
+                {
+                    objectTypeFilter = " and type != 'V' ";
+                }
+            }
+
+            string sql = string.Format(@"WITH cte AS(
+	SELECT
+		a3.name AS [schemaname],
+		a2.name AS [tablename],
+        a2.type,
+        a2.type_desc,
+		a1.rows AS row_count,
+		(a1.reserved + ISNULL(a4.reserved,0))* 8.0 / 1024 AS reserved,
+		a1.data * 8.0 / 1024 AS data,
+		(CASE WHEN (a1.used + ISNULL(a4.used,0)) > a1.data THEN (a1.used + ISNULL(a4.used,0)) - a1.data ELSE 0 END) * 8.0 / 1024 AS index_size,
+		(CASE WHEN (a1.reserved + ISNULL(a4.reserved,0)) > a1.used THEN (a1.reserved + ISNULL(a4.reserved,0)) - a1.used ELSE 0 END) * 8.0 / 1024 AS unused
+	FROM
+		(SELECT ps.object_id
+			, SUM(
+				CASE
+					WHEN (ps.index_id < 2) THEN row_count
+					ELSE 0
+				END) AS [rows]
+			, SUM(ps.reserved_page_count) AS reserved
+            , SUM(
+				CASE
+					WHEN (ps.index_id < 2) THEN (ps.in_row_data_page_count + ps.lob_used_page_count + ps.row_overflow_used_page_count)
+					ELSE (ps.lob_used_page_count + ps.row_overflow_used_page_count)
+				END) AS data
+            , SUM (ps.used_page_count) AS used
+		FROM sys.dm_db_partition_stats ps
+		GROUP BY ps.object_id) AS a1
+	LEFT OUTER JOIN
+		(SELECT it.parent_id
+			, SUM(ps.reserved_page_count) AS reserved
+			, SUM(ps.used_page_count) AS used
+		 FROM sys.dm_db_partition_stats ps
+		    INNER JOIN sys.internal_tables it ON (it.object_id = ps.object_id)
+		 WHERE it.internal_type IN (202,204)
+		 GROUP BY it.parent_id) AS a4 ON (a4.parent_id = a1.object_id)
+	INNER JOIN sys.all_objects a2  ON ( a1.object_id = a2.object_id )
+	INNER JOIN sys.schemas a3 ON (a2.schema_id = a3.schema_id)
+	WHERE a2.type <> N'S' and a2.type <> N'IT'
+)
+select schemaname
+	, tablename
+	, row_count
+    , type
+    , type_desc
+	, reserved
+	, data
+	, index_size
+	, unused
+	, CASE WHEN reserved != 0 THEN index_size * 100.0 / reserved ELSE 0 END AS IndexPercent
+FROM cte
+WHERE reserved >= {0}
+    AND reserved < {1}
+    {2}
+ORDER BY reserved DESC", minSize, maxSize, objectTypeFilter);
+            return d.ExecuteWithResults(sql).Tables[0];
+        }
+
     }
 }
