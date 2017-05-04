4a8f65d0118a53a8f3386c9b4b3bfd6451132bfb
GrÃ©gory Boge <g.boge@criteo.com>
2017-05-04 08:09:27 +0200

    Manage SQL Server last version

---
+++ KVersion.cs
@@ -0,0 +1,34 @@
+﻿namespace KMO
+{
+    public static class KVersion
+    {
+        public const int Sp2017 = 500;
+        public const int Cu2017 = 500;
+        public const int Sp2016 = 4001;
+        public const int Cu2016 = 4422;
+        public const int Sp2014 = 5000;
+        public const int Cu2014 = 5540;
+        public const int Sp2012 = 6020;
+        public const int Cu2012 = 6594;
+        public const int Sp2008R2 = 6000;
+        public const int Cu2008R2 = 6542;
+        public const int Sp2008 = 6000;
+        public const int Cu2008 = 6547;
+        public const int Sp2005 = 5000;
+        public const int Cu2005 = 5324;
+        public const string Sp2017Link = "https://www.microsoft.com/en-us/evalcenter/evaluate-sql-server-2017-ctp";
+        public const string Cu2017Link = "https://www.microsoft.com/en-us/evalcenter/evaluate-sql-server-2017-ctp";
+        public const string Sp2016Link = "https://www.microsoft.com/en-us/download/details.aspx?id=54276";
+        public const string Cu2016Link = "https://support.microsoft.com/en-us/help/4013106";
+        public const string Sp2014Link = "https://www.microsoft.com/en-us/download/details.aspx?id=53168";
+        public const string Cu2014Link = "https://support.microsoft.com/en-us/help/4010394";
+        public const string Sp2012Link = "https://www.microsoft.com/en-us/download/details.aspx?id=49996";
+        public const string Cu2012Link = "https://support.microsoft.com/en-us/help/4013104";
+        public const string Sp2008R2Link = "http://www.microsoft.com/en-us/download/details.aspx?id=44271";
+        public const string Cu2008R2Link = "https://support.microsoft.com/en-us/kb/3146034";
+        public const string Sp2008Link = "http://www.microsoft.com/en-us/download/details.aspx?id=44278";
+        public const string Cu2008Link = "https://support.microsoft.com/en-us/kb/3146034";
+        public const string Sp2005Link = "http://www.microsoft.com/downloads/en/details.aspx?FamilyID=b953e84f-9307-405e-bceb-47bd345baece";
+        public const string Cu2005Link = "http://support.microsoft.com/kb/2716427";
+    }
+}
