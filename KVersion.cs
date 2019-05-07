1530828388294f5f6ec1e6634ef6bc07387c955e
Gregory Boge <g.boge@criteo.com>
2019-05-07 11:33:29 +0200

    Update sql server versions

--- KVersion.cs
+++ KVersion.cs
@@ -3,31 +3,31 @@
     public static class KVersion
     {
         public const int Sp2017 = 1000;
-        public const int Cu2017 = 3008;
-        public const int Sp2016 = 4001;
-        public const int Cu2016 = 4457;
-        public const int Sp2014 = 5000;
-        public const int Cu2014 = 5557;
+        public const int Cu2017 = 3048;
+        public const int Sp2016 = 5026;
+        public const int Cu2016 = 5264;
+        public const int Sp2014 = 6024;
+        public const int Cu2014 = 6214;
         public const int Sp2012 = 7001;
-        public const int Cu2012 = 7001;
+        public const int Cu2012 = 7469;
         public const int Sp2008R2 = 6000;
-        public const int Cu2008R2 = 6542;
+        public const int Cu2008R2 = 6560;
         public const int Sp2008 = 6000;
-        public const int Cu2008 = 6547;
+        public const int Cu2008 = 6556;
         public const int Sp2005 = 5000;
         public const int Cu2005 = 5324;
         public const string Sp2017Link = "https://www.microsoft.com/en-us/sql-server/sql-server-downloads";
-        public const string Cu2017Link = "https://support.microsoft.com/en-us/help/4052574";
-        public const string Sp2016Link = "https://www.microsoft.com/en-us/download/details.aspx?id=54276";
-        public const string Cu2016Link = "https://support.microsoft.com/en-us/help/4037354";
-        public const string Sp2014Link = "https://www.microsoft.com/en-us/download/details.aspx?id=53168";
-        public const string Cu2014Link = "https://support.microsoft.com/en-us/help/4037356";
+        public const string Cu2017Link = "https://support.microsoft.com/en-us/help/4466404";
+        public const string Sp2016Link = "https://www.microsoft.com/en-us/download/details.aspx?id=56836";
+        public const string Cu2016Link = "https://support.microsoft.com/en-us/help/4475776";
+        public const string Sp2014Link = "https://support.microsoft.com/en-us/help/4022619";
+        public const string Cu2014Link = "https://support.microsoft.com/en-us/help/4482960";
         public const string Sp2012Link = "https://www.microsoft.com/en-us/download/details.aspx?id=56040";
-        public const string Cu2012Link = "https://www.microsoft.com/en-us/download/details.aspx?id=56040";
+        public const string Cu2012Link = "https://support.microsoft.com/en-us/help/4091266";
         public const string Sp2008R2Link = "http://www.microsoft.com/en-us/download/details.aspx?id=44271";
-        public const string Cu2008R2Link = "https://support.microsoft.com/en-us/kb/3146034";
+        public const string Cu2008R2Link = "https://support.microsoft.com/en-us/help/4057113";
         public const string Sp2008Link = "http://www.microsoft.com/en-us/download/details.aspx?id=44278";
-        public const string Cu2008Link = "https://support.microsoft.com/en-us/kb/3146034";
+        public const string Cu2008Link = "https://support.microsoft.com/en-us/help/4057114";
         public const string Sp2005Link = "http://www.microsoft.com/downloads/en/details.aspx?FamilyID=b953e84f-9307-405e-bceb-47bd345baece";
         public const string Cu2005Link = "http://support.microsoft.com/kb/2716427";
     }
